f1c90d6d5a818540c3c92a69b291ae2578b8ca71
AdamViant <48057811+AdamViant@users.noreply.github.com>
2021-08-12 14:24:39 +0100

    Add files via upload
    Changes made to support quiz database integration and shuffle on questions and answers.

--- Server/public/views/resources/scripts/quiz.js
+++ Server/public/views/resources/scripts/quiz.js
@@ -16,7 +16,6 @@ const submitQuiz = document.getElementById("submitQuiz");
 const submitScore = document.getElementById("submitScore");
 
 //Question Variables
-var selectedQuiz;
 var questions = new Array();
 var lastQuestionIndex;
 var runningQuestionIndex;
@@ -31,109 +30,125 @@ var TIMER;
 //Score
 var score = 0;
 
+selectedQuiz = quizData.quizId;
+
 //Questions
-function selectQuiz()
-{
-  if(header.innerHTML == "HTML Quiz"){
-    selectedQuiz = 1;
-  }else{
-    if(header.innerHTML == "CSS Quiz"){
-      selectedQuiz = 2;
+questions =
+  [
+    {
+      question: quizData.question1.question,
+      choiceA: quizData.question1.choiceA,
+      choiceB: quizData.question1.choiceB,
+      choiceC: quizData.question1.choiceC,
+      choiceD: quizData.question1.choiceD,
+      correct: quizData.question1.answer
+    },{
+      question: quizData.question2.question,
+      choiceA: quizData.question2.choiceA,
+      choiceB: quizData.question2.choiceB,
+      choiceC: quizData.question2.choiceC,
+      choiceD: quizData.question2.choiceD,
+      correct: quizData.question2.answer
+    },{
+      question: quizData.question3.question,
+      choiceA: quizData.question3.choiceA,
+      choiceB: quizData.question3.choiceB,
+      choiceC: quizData.question3.choiceC,
+      choiceD: quizData.question3.choiceD,
+      correct: quizData.question3.answer
+    },{
+      question: quizData.question4.question,
+      choiceA: quizData.question4.choiceA,
+      choiceB: quizData.question4.choiceB,
+      choiceC: quizData.question4.choiceC,
+      choiceD: quizData.question4.choiceD,
+      correct: quizData.question4.answer
+    },{
+      question: quizData.question5.question,
+      choiceA: quizData.question5.choiceA,
+      choiceB: quizData.question5.choiceB,
+      choiceC: quizData.question5.choiceC,
+      choiceD: quizData.question5.choiceD,
+      correct: quizData.question5.answer
     }
+  ]
+
+  lastQuestionIndex = questions.length - 1;
+  runningQuestionIndex = 0;
+
+//Shuffle Function
+
+function shuffle(array) {
+  var currentIndex = array.length,  randomIndex;
+
+  // While there remain elements to shuffle...
+  while (0 !== currentIndex) {
+
+    // Pick a remaining element...
+    randomIndex = Math.floor(Math.random() * currentIndex);
+    currentIndex--;
+
+    // And swap it with the current element.
+    [array[currentIndex], array[randomIndex]] = [
+      array[randomIndex], array[currentIndex]];
   }
 
-
-  if (selectedQuiz == 1) //Html Quiz
-  {
-    questions = [
-      {
-        question : "What does HTML stand for?",
-        choiceA: "Housed Translation Merit Link",
-        choiceB: "Hot Text Multiple Layers",
-        choiceC: "Hyper Text Markup Language",
-        choiceD: "Halo Terminal Matrix Language",
-        correct: "C"
-      },{
-        question : "What is the first line you should enter in HTML 5?",
-        choiceA: "< !DOCTYPE html >",
-        choiceB: "< html 5 >",
-        choiceC: "< !TYPE html >",
-        choiceD: "< html >",
-        correct: "A"
-      },{
-        question : "Which of the following would produce the text 'hello' on the page?",
-        choiceA: "< p >hello< p >",
-        choiceB: "< p / >hello",
-        choiceC: "< p >hello< / p >",
-        choiceD: "< hello >",
-        correct: "C"
-      },{
-        question : "Which element would be the correct way to insert an image to the page?",
-        choiceA: "< image >",
-        choiceB: "< pic >",
-        choiceC: "< photo >",
-        choiceD: "< img >",
-        correct: "D"
-      },{
-        question : "Which syntax would create a line space between text?",
-        choiceA: "< line / >",
-        choiceB: "< br / >",
-        choiceC: "< break / >",
-        choiceD: "< space / >",
-        correct: "B"
-      }
-    ]
-
-    lastQuestionIndex = questions.length - 1;
-    runningQuestionIndex = 0;
-
-  }else{
-    if(selectedQuiz == 2){//CSS Quiz
-    questions = [
-      {
-        question : "What does CSS stand for?",
-        choiceA: "Created Style Showcase",
-        choiceB: "Chosen Shading Style",
-        choiceC: "Creation and Shading Standards",
-        choiceD: "Cascading Style Sheet",
-        correct: "D"
-      },{
-        question : "What is the correct syntax for an ID in CSS?",
-        choiceA: ".",
-        choiceB: "#",
-        choiceC: "*",
-        choiceD: "%",
-        correct: "B"
-      },{
-        question : "What is the way to input a measurement in pixels? ",
-        choiceA: "px",
-        choiceB: "pixels",
-        choiceC: "pix",
-        choiceD: "[]",
-        correct: "A"
-      },{
-        question : "What is the correct syntax for an Class in CSS?",
-        choiceA: ".",
-        choiceB: "#",
-        choiceC: "*",
-        choiceD: "%",
-        correct: "A"
-      },{
-        question : "What is the correct syntax for inserting internal CSS into HTML",
-        choiceA: "< CSS >",
-        choiceB: "< styling >",
-        choiceC: "< style >",
-        choiceD: "< CSS Style >",
-        correct: "C"
-      }
-    ]
-
-    lastQuestionIndex = questions.length - 1;
-    runningQuestionIndex = 0;
-
-    }
-  }
+  return array;
 }
+
+//Randomsise Question Order
+
+shuffle(questions);
+
+//Randomise the Answer Order for each question
+
+questions.forEach((question, i) => {
+  var tempAnswer = question.correct;
+  var trueAnswer;
+  var tempArray = new Array();
+
+  //Finds Value of the Correct Answer
+  switch(tempAnswer) {
+    case "A":
+      trueAnswer = question.choiceA;
+      break;
+    case "B":
+      trueAnswer = question.choiceB;
+      break;
+    case "C":
+      trueAnswer = question.choiceC;
+      break;
+    case "D":
+      trueAnswer = question.choiceD;
+      break;
+  }
+
+  //Shuffles the Answer Order
+  tempArray = [question.choiceA, question.choiceB, question.choiceC, question.choiceD]
+  shuffle(tempArray);
+
+  //Matches the Correct Answer Post-Shuffle
+  if(trueAnswer == tempArray[0])
+  {
+    tempAnswer = "A";
+  }else if(tempArray[1] == trueAnswer){
+    tempAnswer = "B";
+  }else if(tempArray[2] == trueAnswer){
+    tempAnswer = "C";
+  }else if(tempArray[3] == trueAnswer){
+    tempAnswer = "D";
+  }
+
+  //Outputs new Order with Correct Answer
+  question.correct = tempAnswer;
+  question.choiceA = tempArray[0];
+  question.choiceB = tempArray[1];
+  question.choiceC = tempArray[2];
+  question.choiceD = tempArray[3];
+
+});
+
+
 
 //Render Questions
 function questionRender()
@@ -200,7 +215,6 @@ function scoreRender()
 function startQuiz()
 {
   start.style.display = "none";
-  selectQuiz();
   counterRender();
   TIMER = setInterval(counterRender,1000);
   questionRender();
